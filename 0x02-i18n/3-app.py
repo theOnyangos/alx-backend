b234b04b921eed299b4a8633f204a73a06863fe4
theOnynagos <otienodennis29@gmail.com>
2023-08-14 14:52:18 +0300

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""i18n compliant flask application"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+app = Flask(__name__)
+
+
+# Configure available languages
+class Config:
+    """Config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+# Initialize Babel
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """add list of supported languages to flask's Accept-language header"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index() -> str:
+    """main route"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
